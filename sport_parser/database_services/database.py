b5b5f6c138b521476accdd674e14442fafe85e35
Dmitrij Svetov <bandodok@gmail.com>
2021-09-04 11:29:12 +0300

    refactored get_team_stats_view

--- sport_parser/database_services/database.py
+++ sport_parser/database_services/database.py
@@ -1,6 +1,6 @@
 from sport_parser.khl.models import KHLProtocol
 from django.db.models import Sum
-import time, datetime
+import datetime
 
 
 def add_khl_protocol_to_database(protocol) -> None:
@@ -28,7 +28,7 @@ def add_khl_protocol_to_database(protocol) -> None:
 def get_team_stats_view():
     """Возвращает список с рассчитанной статистикой команд в виде списков"""
     stats = [[
-        'Club',
+        'Team',
         'Sh',
         'Sh(A)',
         'Sh%',
@@ -48,87 +48,91 @@ def get_team_stats_view():
         'Penalty',
         'Hits',
     ]]
-    teams = KHLProtocol.objects.values('team_id').order_by('team_id').distinct()
+    teams = get_team_list()
     for team in teams:
-        t = team['team_id']
-        team_stats = [t, ]
-        match_list = KHLProtocol.objects.filter(team_id=t).values_list('match_id')
+        team_stats = [team, ]
+        match_list = get_match_list(team)
 
-        sh_list = KHLProtocol.objects.filter(match_id__in=match_list).filter(team_id=t).values_list('sh')
-        sh = get_median(sh_list)
-        team_stats.append(format(sh, ".1f"))
+        sh = get_team_stat(team, 'sh', match_list, mode='median')
+        sog = get_team_stat(team, 'sog', match_list, mode='median')
+        g = get_team_stat(team, 'g', match_list, mode='median')
+        blocks = get_team_stat(team, 'blocks', match_list, mode='median')
+        time_a = get_team_stat(team, 'time_a', match_list, mode='median')
+        penalty = get_team_stat(team, 'penalty', match_list, mode='median')
+        hits = get_team_stat(team, 'hits', match_list, mode='median')
 
-        sha_list = KHLProtocol.objects.filter(match_id__in=match_list).exclude(team_id=t).values_list('sh')
-        sha = get_median(sha_list)
-        team_stats.append(format(sha, ".1f"))
+        sha = get_opponent_stat(team, 'sh', match_list, mode='median')
+        soga = get_opponent_stat(team, 'sog', match_list, mode='median')
+        ga = get_opponent_stat(team, 'g', match_list, mode='median')
+        blocksa = get_opponent_stat(team, 'blocks', match_list, mode='median')
+        time_aa = get_opponent_stat(team, 'time_a', match_list, mode='median')
+
+        faceoff = get_team_stat(team, 'faceoff', match_list, mode='sum')
+        faceoffa = get_opponent_stat(team, 'faceoff', match_list, mode='sum')
 
         shp = f'{format(sh / (sh + sha) * 100, ".2f")}%'
-        team_stats.append(shp)
-
-        sog_list = KHLProtocol.objects.filter(match_id__in=match_list).filter(team_id=t).values_list('sog')
-        sog = get_median(sog_list)
-        team_stats.append(format(sog, ".1f"))
-
-        soga_list = KHLProtocol.objects.filter(match_id__in=match_list).exclude(team_id=t).values_list('sog')
-        soga = get_median(soga_list)
-        team_stats.append(format(soga, ".1f"))
-
         sogp = f'{format(sog * 100 / sh, ".2f")}%'
-        team_stats.append(sogp)
-
-        g_list = KHLProtocol.objects.filter(match_id__in=match_list).filter(team_id=t).values_list('g')
-        g = get_median(g_list)
-        team_stats.append(format(g, ".1f"))
-
-        ga_list = KHLProtocol.objects.filter(match_id__in=match_list).exclude(team_id=t).values_list('g')
-        ga = get_median(ga_list)
-        team_stats.append(format(ga, ".1f"))
-
-        faceoff = KHLProtocol.objects.filter(match_id__in=match_list).filter(team_id=t).aggregate(Sum('faceoff'))
-        faceoff = faceoff['faceoff__sum']
-
-        faceoffa = KHLProtocol.objects.filter(match_id__in=match_list).exclude(team_id=t).aggregate(Sum('faceoff'))
-        faceoffa = faceoffa['faceoff__sum']
-
         faceoffp = f'{format(faceoff / (faceoff + faceoffa) * 100, ".2f")}%'
-        team_stats.append(faceoffp)
-
-        blocks_list = KHLProtocol.objects.filter(match_id__in=match_list).filter(team_id=t).values_list('blocks')
-        blocks = get_median(blocks_list)
-        team_stats.append(format(blocks, ".1f"))
-
-        blocksa_list = KHLProtocol.objects.filter(match_id__in=match_list).exclude(team_id=t).values_list('blocks')
-        blocksa = get_median(blocksa_list)
-        team_stats.append(format(blocksa, ".1f"))
-
-        blocksp = f'{round(blocks / (blocks + blocksa) * 100, 2)}%'
-        team_stats.append(blocksp)
-
+        blocksp = f'{format(blocks / (blocks + blocksa) * 100, ".2f")}%'
         devp = f'{format((1 - (soga / sha)) * 100, ".2f")}%'
-        team_stats.append(devp)
-
-        time_a_list = KHLProtocol.objects.filter(match_id__in=match_list).filter(team_id=t).values_list('time_a')
-        time_a = get_median(time_a_list)
-        team_stats.append(sec_to_time(time_a))
-
-        time_aa_list = KHLProtocol.objects.filter(match_id__in=match_list).exclude(team_id=t).values_list('time_a')
-        time_aa = get_median(time_aa_list)
-        team_stats.append(sec_to_time(time_aa))
-
         time_ap = f'{format(time_a / (time_a + time_aa) * 100, ".2f")}%'
-        team_stats.append(time_ap)
 
-        penalty_list = KHLProtocol.objects.filter(match_id__in=match_list).filter(team_id=t).values_list('penalty')
-        penalty = get_median(penalty_list)
-        team_stats.append(format(penalty, ".1f"))
+        time_a = sec_to_time(time_a)
+        time_aa = sec_to_time(time_aa)
 
-        hits_list = KHLProtocol.objects.filter(match_id__in=match_list).filter(team_id=t).values_list('hits')
-        hits = get_median(hits_list)
-        team_stats.append(format(hits, ".1f"))
+        formated_stats = output_format([
+            sh, sha, shp, sog, soga,
+            sogp, g, ga, faceoffp, blocks,
+            blocksa, blocksp, devp, time_a,
+            time_aa, time_ap, penalty, hits
+        ])
+
+        team_stats.extend(formated_stats)
 
         stats.append(team_stats)
 
     return stats
+
+
+def get_team_list():
+    """Возвращает список команд"""
+    teams = KHLProtocol.objects.values('team_id').order_by('team_id').distinct()
+    return [team['team_id'] for team in teams]
+
+
+def get_match_list(team):
+    """Возвращает список матчей match_id команды team"""
+    match_list = KHLProtocol.objects.filter(team_id=team).values_list('match_id')
+    return [match[0] for match in match_list]
+
+
+def get_team_stat(team, stat, match_list, *, mode='median'):
+    """В зависимости от mode возвращает медиану или сумму параметра stat команды team в матчах match_list
+    mode:
+        median - рассчитать медиану
+        sum - рассчитать сумму
+    """
+    stat_list = KHLProtocol.objects.filter(match_id__in=match_list).filter(team_id=team).order_by(stat)
+    return _calculate_stat(stat, stat_list, mode=mode)
+
+
+def get_opponent_stat(team, stat, match_list, *, mode='median'):
+    """В зависимости от mode возвращает медиану или сумму параметра stat противника команды team в матчах match_list
+    mode:
+        median - рассчитать медиану
+        sum - рассчитать сумму
+    """
+    stat_list = KHLProtocol.objects.filter(match_id__in=match_list).exclude(team_id=team).order_by(stat)
+    return _calculate_stat(stat, stat_list, mode=mode)
+
+
+def _calculate_stat(stat, stat_list, mode='median'):
+    if mode == 'median':
+        return get_median(stat_list.values_list(stat))
+    if mode == 'sum':
+        calc_stat = stat_list.aggregate(Sum(stat))
+        return calc_stat[f'{stat}__sum']
+    raise ValueError('Invalid mode')
 
 
 def get_median(item_list):
@@ -148,12 +152,25 @@ def get_median(item_list):
 
 
 def time_to_sec(time):
+    """Возвращает время в секундах"""
     return time.hour * 3600 + time.minute * 60 + time.second
 
 
 def sec_to_time(time):
+    """Возвращает время в виде строки в формате HH:MM"""
     min = int(time // 60)
     sec = int(time - min * 60)
     if sec < 10:
         sec = f'0{sec}'
     return f'{min}:{sec}'
+
+
+def output_format(items):
+    """Принимает список значений и возвращает отформатированный для вывода список"""
+    out = []
+    for item in items:
+        if type(item) == str:
+            out.append(item)
+            continue
+        out.append(format(item, ".1f"))
+    return out
