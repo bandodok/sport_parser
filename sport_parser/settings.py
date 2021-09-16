2557f6de5f42a8443890527d90a2788831a1cfee
Dmitrij Svetov <bandodok@gmail.com>
2021-09-16 23:50:56 +0300

    update secret key usage

--- sport_parser/settings.py
+++ sport_parser/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^kvyxbn+00$52&-r&i3ll*49p)$owdk1)rsncje=b1=vm8ai$o'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
