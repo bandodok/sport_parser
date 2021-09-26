bf18133390034c34e250067e3b09ff4c5bfb502c
Dmitrij Svetov <bandodok@gmail.com>
2021-09-26 14:04:57 +0300

    update team view render context

--- sport_parser/khl/views.py
+++ sport_parser/khl/views.py
@@ -1,5 +1,5 @@
 from sport_parser.khl.database_services.db_add import last_updated
-from sport_parser.khl.database_services.db_get import get_team_attr
+from sport_parser.khl.database_services.db_get import get_team_attr, get_team_id_by_season
 from sport_parser.khl.parsers.match_protocol import parse_season, update_protocols
 from sport_parser.khl.parsers.team_info import parse_teams
 from sport_parser.khl.parsers.score_table import get_score_table
@@ -42,6 +42,7 @@ def team(request, team_id):
     conference = get_team_attr(team_id, 'conference')
     logo = get_team_attr(team_id, 'img')
     season = get_team_attr(team_id, 'season')
+    season_dict = get_team_id_by_season(team_id)
     return render(request, 'khl_team.html', context={
         'stats': team_stats,
         'team': team,
@@ -50,7 +51,8 @@ def team(request, team_id):
         'division': division,
         'conference': conference,
         'logo': logo,
-        'season': season
+        'season': season,
+        'seasons': season_dict
     })
 
 
