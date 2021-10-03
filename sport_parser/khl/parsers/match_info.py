33f2c3a5396a2c2adece0d170a834b9e7248a6a4
Dmitrij Svetov <bandodok@gmail.com>
2021-10-03 20:43:39 +0300

    updated match_info func
    get_khl_match_info renamed to get_khl_season_match_info
    added get_finished_match_info func
    added _date_format func
    updated month_to_int_replace func

--- sport_parser/khl/parsers/match_info.py
+++ sport_parser/khl/parsers/match_info.py
@@ -1,46 +1,115 @@
 from django.conf import settings
 
-from sport_parser.khl.parsers.request import get_request_content
+from sport_parser.khl.parsers.request import get_selenium_content, get_request_content
 
 
-def get_khl_match_info(match_id):
-    """ """
-    url = f"https://text.khl.ru/text/{match_id}.html"
-    soup = get_request_content(url)
+def get_khl_season_match_info(season, webdriver=get_selenium_content):
+    s = settings.SEASONS.get(str(season), 0)
+    if s == 0:
+        return f'season {s} not found'
 
-    team_info = soup.find('div', class_='b-content_section m-match_info').find_all('span')
-    datetime = str(team_info[2]).split('<br/>')
-    time = datetime[1][:-7]
-    date = datetime[0][6:].split(' ')[::-1]
-    month = month_to_int_replace(date[1])
-    full_date = f'{date[0]}-{month}-{date[2]} {time}'
+    output = []
 
-    arena_viewers = team_info[4]
-    viewers = str(arena_viewers).split('<br/>')[1].split(' ')[0]
-    if viewers == '</span>':
-        viewers = 0
-    arena_city = str(arena_viewers).split('<br/>')[0][6:].split('(')
-    arena = arena_city[0].strip()
-    city = arena_city[1][:-1]
+    url = f'https://www.khl.ru/calendar/{s}/00/'
+    soup = webdriver(url)
+    match_soup = soup.find('div', id='tab-calendar-all')
 
+    dates = match_soup.find_all('div', class_='b-final_cup_date')
+    dates = [date.b.text for date in dates]
+
+    matches = match_soup.find_all('div', class_='m-future')
+    match_dict = {date: match_soup for date, match_soup in zip(dates, matches)}
+    for date, matches in match_dict.items():
+        match_list = matches.find_all('li', class_='b-wide_tile_item')
+        new_date = _date_format(date)
+        for match in match_list:
+            href = match.find('dl', class_='b-title-option').div.div.ul.li.a['href']
+            match_id = href.split('/')[3]
+            home_team = match.find('dl', class_='b-details m-club').dd.h5.a.text
+            guest_team = match.find('dl', class_='b-details m-club m-rightward').dd.h5.a.text
+            score = match.find('dl', class_='b-score')
+            if '—' in score.dt.h3.text:
+                finished = True
+                match_info = get_finished_match_info(match_id)
+            else:
+                finished = False
+                time = score.dt.h3.text.split(' ')[0]
+                city = score.dd.p.text
+                arena = ''
+                viewers = 0
+                match_info = {
+                    'arena': arena,
+                    'city': city,
+                    'time': time,
+                    'viewers': viewers
+                }
+            match_info.update({
+                'finished': finished,
+                'match_id': match_id,
+                'date': new_date,
+                'home_team': home_team,
+                'guest_team': guest_team,
+                'season': season
+            })
+            output.append(match_info)
+    return output
+
+
+def get_finished_match_info(match_id):
     season = 0
     for s, id in settings.SEASONS_FIRST_MATCH.items():
-        if id <= match_id:
-            season = s
+        if id <= int(match_id):
+            season = settings.SEASONS.get(s)
             break
-    return [match_id, full_date, season, arena, city, viewers]
+
+    url = f'https://www.khl.ru/game/{season}/{match_id}/preview/'
+    soup = get_request_content(url)
+    extra_info = soup.find_all('li', class_="b-match_add_info_item")
+
+    date_info = extra_info[0]
+    arena_info = extra_info[1]
+
+    info = date_info.find_all('span')[1]
+    info = str(info).split('<br/>')
+    time = info[1][:5]
+
+    info = arena_info.find_all('span')[1]
+
+    info = str(info).split('<br/>')
+    arena_city = info[0][6:]
+    arena, city = arena_city.split(',')
+    viewers = info[1][:-16]
+    city = city.strip()
+    return {
+        'arena': arena,
+        'city': city,
+        'time': time,
+        'viewers': viewers
+    }
+
+
+def _date_format(date):
+    splitted_date = date.split(' ')[:-1]
+    if not splitted_date[0]:
+        splitted_date.pop(0)
+    day, month, year = splitted_date
+    if len(day) == 1:
+        day = f'0{day}'
+    month = month_to_int_replace(month)
+    year = year[:-1]
+    return f'{year}-{month}-{day}'
 
 
 def month_to_int_replace(month: str):
     """Возвращает номер месяца по слову"""
     months = {
-        'янв.': '01',
-        'февр.': '02',
+        'января': '01',
+        'февраля': '02',
         'марта': '03',
-        'авг.': '08',
-        'сент.': '09',
-        'окт.': '10',
-        'нояб.': '11',
-        'дек.': '12'
+        'августа': '08',
+        'сентября': '09',
+        'октября': '10',
+        'ноября': '11',
+        'декабря': '12'
     }
     return months.get(month)
