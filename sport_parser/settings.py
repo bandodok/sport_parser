00e95c2aa20e37693d46fefa084050731291b656
Dmitrij Svetov <bandodok@gmail.com>
2021-10-05 20:55:16 +0300

    moved db from sqlite to postgres

--- sport_parser/settings.py
+++ sport_parser/settings.py
@@ -86,11 +86,12 @@ WSGI_APPLICATION = 'sport_parser.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'timeout': 200,
-        }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
