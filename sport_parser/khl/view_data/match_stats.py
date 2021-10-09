3d101b7f5787f016185fd0243cd2f881c7de1987
Dmitrij Svetov <bandodok@gmail.com>
2021-10-09 20:09:19 +0300

    updated get_match_stats_view

--- sport_parser/khl/view_data/match_stats.py
+++ sport_parser/khl/view_data/match_stats.py
@@ -1,24 +1,73 @@
-from sport_parser.khl.database_services.db_get import get_match_by_id
+from sport_parser.khl.database_services.db_get import get_match_by_id, get_team_season_stats
+from sport_parser.khl.view_data.team_stats import get_team_stats_view
 
 
 def get_match_stats_view(match_id):
     match = get_match_by_id(match_id)
     team1, team2 = match.teams.all()
 
+    team1_season_stats = [team1.name, team1.id]
+    team2_season_stats = [team2.name, team2.id]
+
+    team1_season_stats.extend(get_team_season_stats(team1.id))
+    team2_season_stats.extend(get_team_season_stats(team2.id))
+
+    season_stats = [[
+        'Team',
+        'Sh',
+        'Sh(A)',
+        'Sh%',
+        'SoG',
+        'SoG(A)',
+        'AQ',
+        'G',
+        'G(A)',
+        'FaceOff%',
+        'TimeA',
+        'TimeA(A)',
+        'TimeA%',
+        'DEV%',
+        'PDO%',
+        'Hits',
+        'Blocks',
+        'Blocks(A)',
+        'Blocks%',
+        'Penalty',
+    ],
+        team1_season_stats,
+        team2_season_stats
+    ]
+
     match_info = {
         'date': match.date,
         'time': match.time,
         'arena': match.arena,
-        'viewers': match.viewers
+        'viewers': match.viewers,
+        'season_stats': season_stats,
+        'finished': match.finished
     }
+
+    exclude = 0
+    if match_info['finished']:
+        exclude = match.match_id
+
+    team1_last_matches_query = team1.last_matches(5, exclude=exclude)
+    team2_last_matches_query = team2.last_matches(5, exclude=exclude)
+
+    team1_last_matches = _last_matches_info(team1_last_matches_query)
+    team2_last_matches = _last_matches_info(team2_last_matches_query)
 
     team1_info = {
         'name': team1.name,
         'city': team1.city,
+        'stats': get_team_stats_view(team1.id),
+        'last_matches': team1_last_matches
     }
     team2_info = {
         'name': team2.name,
         'city': team2.city,
+        'stats': get_team_stats_view(team2.id),
+        'last_matches': team2_last_matches
     }
 
     return {
@@ -26,3 +75,17 @@ def get_match_stats_view(match_id):
         'team1_info': team1_info,
         'team2_info': team2_info
     }
+
+
+def _last_matches_info(matches):
+    last_matches = {}
+    for match in matches:
+        last_matches[match.match_id] = {
+            'date': match.date,
+            'time': match.time,
+            'team1_name': match.khlprotocol_set.all()[0].team_id.name,
+            'team1_score': match.khlprotocol_set.all()[0].g,
+            'team2_name': match.khlprotocol_set.all()[1].team_id.name,
+            'team2_score': match.khlprotocol_set.all()[1].g,
+        }
+    return last_matches
--- tests/test_match_stats.py
+++ tests/test_match_stats.py
@@ -1,4 +1,5 @@
-from datetime import datetime, timezone, time
+from datetime import timezone
+import datetime
 
 import pytest
 from fixtures.db_fixture import update_db
@@ -8,20 +9,94 @@ from sport_parser.khl.view_data.match_stats import get_match_stats_view
 @pytest.mark.django_db(transaction=True)
 def test_get_match_stats_view(update_db):
     data = get_match_stats_view(22)
+
     output = {
         'match_info': {
             'arena': 'arena',
-            'date': datetime(2017, 8, 20, 21, 0, tzinfo=timezone.utc),
-            'time': time(15, 0),
-            'viewers': 228
+            'date': datetime.datetime(2017, 8, 28, 21, 0, tzinfo=timezone.utc),
+            'time': datetime.time(15, 0),
+            'viewers': 228,
+            'finished': True,
+            'season_stats': [
+                ['Team', 'Sh', 'Sh(A)', 'Sh%', 'SoG', 'SoG(A)', 'AQ', 'G', 'G(A)', 'FaceOff%', 'TimeA',
+                 'TimeA(A)', 'TimeA%', 'DEV%', 'PDO%', 'Hits', 'Blocks', 'Blocks(A)', 'Blocks%', 'Penalty'],
+                ['test1', 1, '55.0', '55.0', '50.00%', '26.0', '26.0', '47.27%', '6.0', '4.0', '50.00%',
+                 '9:39', '9:39', '50.00%', '52.73%', '97.27%', '15.0', '18.0', '18.0', '50.00%', '8.0'],
+                ['test6', 6, '55.0', '55.0', '50.00%', '26.0', '26.0', '47.27%', '5.0', '5.5', '50.00%',
+                 '9:39', '9:39', '50.00%', '52.73%', '97.27%', '15.0', '18.0', '18.0', '50.00%', '8.0']
+            ],
         },
         'team1_info': {
             'city': 'city',
-            'name': 'test1'
+            'name': 'test1',
+            'stats': [['Sh', 'SoG', 'G', 'Blocks', 'Penalty', 'Hits', 'TimeA', 'Sh(A)', 'SoG(A)', 'G(A)', 'Blocks(A)',
+                       'Penalty(A)', 'Hits(A)', 'TimeA(A)'],
+                      [44, 22, 4, 22, 4, 16, 521, 66, 30, 0, 14, 12, 14, 637],
+                      [44, 22, 8, 22, 4, 16, 521, 66, 30, 5, 14, 12, 14, 637],
+                      [66, 30, 10, 14, 12, 14, 637, 44, 22, 8, 22, 4, 16, 521],
+                      [66, 30, 1, 14, 12, 14, 637, 44, 22, 3, 22, 4, 16, 521]],
+            'last_matches': {
+                15: {
+                    'date': datetime.datetime(2017, 8, 20, 21, 0, tzinfo=timezone.utc),
+                    'team1_name': 'test1',
+                    'team1_score': 4,
+                    'team2_name': 'test2',
+                    'team2_score': 0,
+                    'time': datetime.time(15, 0),
+                },
+                17: {
+                    'date': datetime.datetime(2017, 8, 23, 21, 0, tzinfo=timezone.utc),
+                    'team1_name': 'test1',
+                    'team1_score': 8,
+                    'team2_name': 'test6',
+                    'team2_score': 5,
+                    'time': datetime.time(15, 0)
+                },
+                19: {
+                    'date': datetime.datetime(2017, 8, 25, 21, 0, tzinfo=timezone.utc),
+                    'team1_name': 'test2',
+                    'team1_score': 8,
+                    'team2_name': 'test1',
+                    'team2_score': 10,
+                    'time': datetime.time(15, 0)
+                }
+            }
         },
         'team2_info': {
             'city': 'city',
-            'name': 'test2'
+            'name': 'test6',
+            'stats': [['Sh', 'SoG', 'G', 'Blocks', 'Penalty', 'Hits', 'TimeA', 'Sh(A)', 'SoG(A)', 'G(A)', 'Blocks(A)',
+                       'Penalty(A)', 'Hits(A)', 'TimeA(A)'],
+                      [66, 30, 5, 14, 12, 14, 637, 44, 22, 8, 22, 4, 16, 521],
+                      [66, 30, 5, 14, 12, 14, 637, 44, 22, 8, 22, 4, 16, 521],
+                      [44, 22, 8, 22, 4, 16, 521, 66, 30, 3, 14, 12, 14, 637],
+                      [44, 22, 3, 22, 4, 16, 521, 66, 30, 1, 14, 12, 14, 637]],
+            'last_matches': {
+                17: {
+                    'date': datetime.datetime(2017, 8, 23, 21, 0, tzinfo=timezone.utc),
+                    'team1_name': 'test1',
+                    'team1_score': 8,
+                    'team2_name': 'test6',
+                    'team2_score': 5,
+                    'time': datetime.time(15, 0)
+                },
+                18: {
+                    'date': datetime.datetime(2017, 8, 24, 21, 0, tzinfo=timezone.utc),
+                    'team1_name': 'test2',
+                    'team1_score': 8,
+                    'team2_name': 'test6',
+                    'team2_score': 5,
+                    'time': datetime.time(15, 0)
+                },
+                20: {
+                    'date': datetime.datetime(2017, 8, 26, 21, 0, tzinfo=timezone.utc),
+                    'team1_name': 'test6',
+                    'team1_score': 8,
+                    'team2_name': 'test2',
+                    'team2_score': 3,
+                    'time': datetime.time(15, 0)
+                }
+            }
         }
     }
     assert data == output
