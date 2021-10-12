467e922b07781accea0c50025e40f328f11f5159
Dmitrij Svetov <bandodok@gmail.com>
2021-10-12 23:20:47 +0300

    refactored team_stats_view
    moved get_team_stats to db_get
    added to get_team_stats_view context info from view
    created last_matches_info
    created future_matches_info

--- sport_parser/khl/database_services/db_get.py
+++ sport_parser/khl/database_services/db_get.py
@@ -230,3 +230,35 @@ def get_match_stats(match_id):
     match_stats.extend([team1_stats, team2_stats])
 
     return match_stats
+
+
+def get_team_chart_stats(team_id):
+    output_stats = [[
+
+        'Sh',
+        'SoG',
+        'G',
+        'Blocks',
+        'Penalty',
+        'Hits',
+        'TimeA',
+
+        'Sh(A)',
+        'SoG(A)',
+        'G(A)',
+        'Blocks(A)',
+        'Penalty(A)',
+        'Hits(A)',
+        'TimeA(A)'
+    ]]
+
+    team_stats = get_team_stats_per_day(team_id, 'sh', 'sog', 'g', 'blocks', 'penalty', 'hits', 'time_a')
+    opponent_stats = get_opp_stats_per_day(team_id, 'sh', 'sog', 'g', 'blocks', 'penalty', 'hits', 'time_a')
+
+    for index, value, in enumerate(team_stats):
+        value.extend(opponent_stats[index])
+        value[6] = time_to_sec(value[6])
+        value[13] = time_to_sec(value[13])
+        output_stats.append(value)
+
+    return output_stats
