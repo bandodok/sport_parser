6660289ab49ce46bc239822b8c63a8f67fb72ee2
Dmitrij Svetov <bandodok@gmail.com>
2021-10-16 15:31:20 +0300

    updated get_calendar_view output to json format

--- sport_parser/khl/view_data/calendar.py
+++ sport_parser/khl/view_data/calendar.py
@@ -1,3 +1,7 @@
+import json
+from django.core.serializers.json import DjangoJSONEncoder
+
+
 from sport_parser.khl.database_services.db_get import get_matches_by_season
 from sport_parser.khl.view_data.team_stats import last_matches_info, future_matches_info
 
@@ -7,9 +11,10 @@ def get_calendar_view(season):
     matches = get_matches_by_season(season)
     finished_matches = last_matches_info(matches.filter(finished=True).order_by('date'))
     unfinished_matches = future_matches_info(matches.filter(finished=False).order_by('date'))
+
     return {
         'season': season,
-        'finished_matches': finished_matches,
-        'unfinished_matches': unfinished_matches,
+        'finished_matches': json.dumps(finished_matches, cls=DjangoJSONEncoder),
+        'unfinished_matches': json.dumps(unfinished_matches, cls=DjangoJSONEncoder),
     }
 
