bb0ae3b24ef3429a63010866dc669f0de76d9b79
Dmitrij Svetov <bandodok@gmail.com>
2021-11-29 20:05:09 +0300

    removed unused views, added Updater to parser views

--- sport_parser/khl/urls.py
+++ sport_parser/khl/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from sport_parser.khl.views import update_finished, stats, team, match, update_teams, update_protocol, \
-    update_season_matches, calendar
+from sport_parser.khl.views import stats, team, match, update_protocol, update_season_matches, calendar
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
@@ -10,9 +9,7 @@ urlpatterns = [
     path('match/<int:match_id>/', match, name='match'),
     path('calendar/<int:season>/', calendar, name='calendar'),
 
-    path('update/teams/', update_teams),
     path('update/season_matches/<int:season>', update_season_matches),
-    path('update/finished_protocol/<int:season>/', update_finished),
     path('update/protocol/', update_protocol),
 ]
 
