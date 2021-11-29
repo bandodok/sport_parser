42a48e20634b24c613b2a75be8d29a050ff662c1
Dmitrij Svetov <bandodok@gmail.com>
2021-11-29 20:01:02 +0300

    updated tests for new classes

---
+++ tests/test_db.py
@@ -0,0 +1,53 @@
+import pytest
+
+from fixtures.db_fixture import get_protocol, get_teams, get_matches
+from sport_parser.khl.database_services.db_add import add_khl_protocol_to_database, add_teams_to_database, \
+    add_matches_to_database, last_updated
+from sport_parser.khl.models import KHLProtocol, KHLTeams, KHLMatch, KHLSeason
+from sport_parser.khl.db import DB
+
+
+@pytest.mark.django_db(transaction=True)
+def test_add_team(get_teams):
+    db = DB()
+    db.add_team(get_teams[0])
+    team = KHLTeams.objects.get(name='test1')
+    assert team.img == 'img'
+    assert team.city == 'city'
+    assert team.arena == 'arena'
+    assert team.division == 'division'
+    assert team.conference == 'conference'
+    assert team.season.id == 21
+
+
+@pytest.mark.django_db(transaction=True)
+def test_add_match(get_teams, get_matches):
+    db = DB()
+    db.add_team(get_teams[0])
+    db.add_team(get_teams[1])
+    for match in get_matches:
+        db.add_match(match)
+    match = KHLMatch.objects.get(id=12)
+    assert match.season.id == 21
+    assert match.arena == 'arena'
+    assert match.city == 'city'
+
+
+@pytest.mark.django_db(transaction=True)
+def test_add_protocol(get_teams, get_matches, get_protocol):
+    db = DB()
+    db.add_team(get_teams[0])
+    db.add_team(get_teams[1])
+    for match in get_matches:
+        db.add_match(match)
+    team1_id = KHLTeams.objects.get(name='test1').id
+    team2_id = KHLTeams.objects.get(name='test2').id
+    db.add_protocol(get_protocol)
+    team1 = KHLProtocol.objects.get(team=team1_id)
+    team2 = KHLProtocol.objects.get(team=team2_id)
+    assert team1.g == 4
+    assert team2.g == 0
+    assert team1.sh == 44
+    assert team2.sh == 66
+    assert team1.sog == 22
+    assert team2.sog == 30
