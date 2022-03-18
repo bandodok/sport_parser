6e7d1f79b183b5bcccc55abc13afea859379c679
Dmitrij Svetov <bandodok@gmail.com>
2022-03-18 22:32:12 +0300

    added superuser and celery beat schedule creation in init_db.py

--- init_db.py
+++ init_db.py
@@ -1,5 +1,6 @@
 import os
 import django
+from django.utils import timezone
 
 
 def main():
@@ -8,6 +9,37 @@ def main():
 
     from sport_parser.khl.models import KHLSeason
     from sport_parser.nhl.models import NHLSeason
+    from django.contrib.auth.models import User
+    from django_celery_beat.models import PeriodicTask, IntervalSchedule
+
+    # Create superuser
+    try:
+        User.objects.get(username=os.getenv('ADMIN_USERNAME'))
+    except django.contrib.auth.models.User.DoesNotExist:
+        User.objects.create_superuser(
+            username=os.getenv('ADMIN_USERNAME'),
+            password=os.getenv('ADMIN_PASSWORD'),
+        )
+
+    # Default celery beat schedule tasks
+    interval, _ = IntervalSchedule.objects.get_or_create(every=10, period='minutes')
+    update_list = [
+        "khl",
+        "nhl"
+    ]
+    for item in update_list:
+        try:
+            PeriodicTask.objects.get_or_create(name=f'{item}_update')
+        except django.core.exceptions.ValidationError:
+            PeriodicTask.objects.get_or_create(
+                name=f'{item}_update',
+                interval_id=interval.id,
+                task='update',
+                args=f'["{item}"]',
+                start_time=timezone.now(),
+                one_off=False,
+                enabled=True
+            )
 
     # KHL season settings
     KHLSeason.objects.get_or_create(id=21, external_id=1097)
