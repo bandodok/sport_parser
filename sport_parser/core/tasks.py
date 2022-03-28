6f91994bf6b77f4f3e059f1a0ac2b6f5579453d3
Dmitrij Svetov <bandodok@gmail.com>
2022-03-28 18:22:56 +0300

    created schedule_live_match task

--- sport_parser/core/tasks.py
+++ sport_parser/core/tasks.py
@@ -1,6 +1,8 @@
 from celery import shared_task
 
 from sport_parser.core.creator import Creator
+from sport_parser.core.models import LiveMatches
+from django_celery_beat.models import PeriodicTask
 
 
 @shared_task(name='update')
@@ -15,3 +17,13 @@ def parse_season(config, season_id):
     creator = Creator(config)
     updater = creator.get_updater()
     updater.parse_season(season_id)
+
+
+@shared_task(name='schedule_live_match')
+def schedule_live_match(league, match_id):
+    LiveMatches.objects.get_or_create(
+        league=league,
+        match_id=match_id
+    )
+    PeriodicTask.objects.get(name=f'{league}_{match_id}_live_match').delete()
+
