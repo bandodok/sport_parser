c416a8c3eed474426bca09fc2350e2b1d37dd986
Dmitrij Svetov <bandodok@gmail.com>
2022-03-28 20:34:07 +0300

    added queues to tasks

--- sport_parser/core/tasks.py
+++ sport_parser/core/tasks.py
@@ -5,21 +5,21 @@ from sport_parser.core.models import LiveMatches
 from django_celery_beat.models import PeriodicTask
 
 
-@shared_task(name='update')
+@shared_task(name='update', queue='regular_update')
 def update(config):
     creator = Creator(config)
     updater = creator.get_updater()
     updater.update()
 
 
-@shared_task(name='parse_season')
+@shared_task(name='parse_season', queue='regular_update')
 def parse_season(config, season_id):
     creator = Creator(config)
     updater = creator.get_updater()
     updater.parse_season(season_id)
 
 
-@shared_task(name='schedule_live_match')
+@shared_task(name='schedule_live_match', queue='regular_update')
 def schedule_live_match(league, match_id):
     LiveMatches.objects.get_or_create(
         league=league,
