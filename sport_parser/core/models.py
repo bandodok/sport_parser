c0e11b32d92b82570b0bf106abb51a6f1f692810
Dmitrij Svetov <bandodok@gmail.com>
2022-03-28 18:22:15 +0300

    created live match data model

--- sport_parser/core/models.py
+++ sport_parser/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import json
 
-# Create your models here.
+
+class LiveMatches(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    league = models.CharField(max_length=100)
+    match_id = models.IntegerField()
+    match_data = models.JSONField(null=True, encoder=json.JSONEncoder, decoder=json.JSONDecoder)
