c38cdafc0b643c203c29f466e79a0c21e44df82f
Dmitrij Svetov <bandodok@gmail.com>
2022-04-03 16:27:19 +0300

    added default data to live match model

--- sport_parser/core/models.py
+++ sport_parser/core/models.py
@@ -7,4 +7,15 @@ class LiveMatches(models.Model):
     updated = models.DateTimeField(auto_now=True)
     league = models.CharField(max_length=100)
     match_id = models.IntegerField()
-    match_data = models.JSONField(null=True, encoder=json.JSONEncoder, decoder=json.JSONDecoder)
+    match_data = models.JSONField(
+        null=True,
+        encoder=json.JSONEncoder,
+        decoder=json.JSONDecoder,
+        default=dict(
+            match_status='матч скоро начнется',
+            team_1_score='-',
+            team_2_score='-',
+            data={
+                'row_home': '',
+                'row_guest': ''
+            }))
