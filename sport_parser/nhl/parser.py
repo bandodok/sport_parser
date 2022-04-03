5fcfb1ce7522280d6553bb6e22db18174871cf6e
Dmitrij Svetov <bandodok@gmail.com>
2022-04-03 16:16:12 +0300

    refactored nhl parser, added parse_live_protocol

--- sport_parser/nhl/parser.py
+++ sport_parser/nhl/parser.py
@@ -121,16 +121,12 @@ class NHLParser(Parser):
         return match_info
 
     def parse_protocol(self, match):
-        url = f'https://statsapi.web.nhl.com/api/v1/game/{match.id}/feed/live'  # 2021010003
+        match_data = self._get_match_data(match.id)
 
-        match_data = self.get_api_request_content(url)
-        live_data = match_data.get('liveData')
-        plays_data = match_data.get('liveData').get('plays').get('allPlays')
+        home_team = match_data['liveData']['boxscore']['teams']['home']['team']['name']
+        guest_team = match_data['liveData']['boxscore']['teams']['away']['team']['name']
 
-        boxscore = live_data['boxscore']
-        home_stats = boxscore['teams']['home']['teamStats']['teamSkaterStats']
-        guest_stats = boxscore['teams']['away']['teamStats']['teamSkaterStats']
-
+        # статистика из основного раздела
         stat_dict = {
             'g': 'goals',
             'sog': 'shots',
@@ -143,85 +139,72 @@ class NHLParser(Parser):
             'takeaways': 'takeaways',
             'giveaways': 'giveaways'
         }
-
-        home_protocol = {
-            'team': boxscore['teams']['home']['team']['name'],
-            'match_id': match.id,
-            'g_b': 0,
-        }
-        guest_protocol = {
-            'team': boxscore['teams']['away']['team']['name'],
-            'match_id': match.id,
-            'g_b': 0,
-        }
-
-        for model_key, api_key in stat_dict.items():
-            home_protocol[model_key] = home_stats[api_key]
-            guest_protocol[model_key] = guest_stats[api_key]
-
-        linescore = live_data['linescore']
+        main_data = self._get_main_data(match_data, stat_dict)
 
         # счет по периодам
-        for period in linescore['periods']:
-            num = period["num"]
-            if num == 4:
-                num = 'ot'
-            home_protocol[f'g_{num}'] = period['home']['goals']
-            guest_protocol[f'g_{num}'] = period['away']['goals']
-
-        # +1 гол выигравшему по буллитам
-        home_b = linescore.get('shootoutInfo').get('home').get('scores')
-        guest_b = linescore.get('shootoutInfo').get('away').get('scores')
-        if home_b > guest_b:
-            home_protocol['g_b'] += 1
-        if home_b < guest_b:
-            guest_protocol['g_b'] += 1
+        periods_data = self._get_score_by_period(match_data)
 
         # сбор статистики из всех ивентов матча
-        events = {
-            'home': {},
-            'guest': {}
-        }
+        event_data = self._get_events_data(match_data, home_team, guest_team)
 
-        for event in plays_data:
-            event_name = event['result']['event']
-            team = event.get('team')
-            if not team:
-                continue
-            team = team.get('name')
+        home_protocol = main_data['row_home'] | periods_data['row_home'] | event_data['row_home']
+        guest_protocol = main_data['row_guest'] | periods_data['row_guest'] | event_data['row_guest']
 
-            if team == home_protocol['team']:
-                event_team = 'home'
-            elif team == guest_protocol['team']:
-                event_team = 'guest'
-            else:
-                event_team = 'unknown'
-
-            if event_name in events[event_team]:
-                events[event_team][event_name] += 1
-            else:
-                events[event_team][event_name] = 1
-
-        home_protocol.update({
-            'faceoff': events['home'].get('Faceoff'),
-            'sh': sum((
-                events['home'].get('Shot') or 0,
-                events['home'].get('Goal') or 0,
-                events['home'].get('Blocked Shot') or 0,
-                events['home'].get('Missed Shot') or 0
-            )),
-        })
-        guest_protocol.update({
-            'faceoff': events['guest'].get('Faceoff'),
-            'sh': sum((
-                events['guest'].get('Shot') or 0,
-                events['guest'].get('Goal') or 0,
-                events['guest'].get('Blocked Shot') or 0,
-                events['guest'].get('Missed Shot') or 0
-            )),
-        })
+        home_protocol['match_id'] = match.id
+        guest_protocol['match_id'] = match.id
 
         return home_protocol, guest_protocol
+
+    def parse_live_protocol(self, match_id):
+        match_data = self._get_match_data(match_id)
+
+        match_status = self._get_match_status(match_data)
+        if match_status == 'матч скоро начнется':
+            return {
+                'match_status': match_status,
+                'team_1_score': '-',
+                'team_2_score': '-',
+                'data': {
+                    'row_home': '',
+                    'row_guest': ''
+                }
+            }
+
+        home_team = match_data['liveData']['boxscore']['teams']['home']['team']['name']
+        guest_team = match_data['liveData']['boxscore']['teams']['away']['team']['name']
+
+
+
+        # статистика из основного раздела
+        stat_dict = {
+            'g': 'goals',
+            'sog': 'shots',
+            'penalty': 'pim',
+            'blocks': 'blocked',
+            'hits': 'hits',
+            'takeaways': 'takeaways',
+            'giveaways': 'giveaways'
+        }
+        main_data = self._get_main_data(match_data, stat_dict)
+
+        # сбор статистики из всех ивентов матча
+        event_data = self._get_events_data(match_data, home_team, guest_team)
+
+        home_protocol = main_data['row_home'] | event_data['row_home']
+        guest_protocol = main_data['row_guest'] | event_data['row_guest']
+
+        home_protocol['match_id'] = match_id
+        guest_protocol['match_id'] = match_id
+
+        return {
+            'match_status': match_status,
+            'team_1_score': home_protocol.get('g', 0),
+            'team_2_score': guest_protocol.get('g', 0),
+            'data': {
+                'row_home': home_protocol,
+                'row_guest': guest_protocol
+            }
+        }
 
     def get_picture(self, team_name: str):
         updated_team_name = team_name.replace(' ', '').lower()
@@ -234,3 +217,111 @@ class NHLParser(Parser):
         if not img.startswith('http'):
             img = f'https:{img}'
         return img
+
+    def _get_match_data(self, match_id):
+        url = f'https://statsapi.web.nhl.com/api/v1/game/{match_id}/feed/live'  # 2021010003
+        return self.get_api_request_content(url)
+
+    @staticmethod
+    def _get_match_status(match_data):
+        status = match_data['gameData']['status']
+        if status['detailedState'] == 'Pre-Game':
+            return 'матч скоро начнется'
+        if status['detailedState'] in ('In Progress', 'In Progress - Critical'):
+            status = match_data['liveData']['linescore']
+            if status['currentPeriodTimeRemaining'] == 'END':
+                return f"{status['currentPeriod']}-й перерыв"
+            if status['currentPeriod'] == 4:
+                return 'овертайм'
+            return f"{status['currentPeriod']}-й период"
+        if status['detailedState'] == 'Game Over':
+            return 'завершение'
+        if status['detailedState'] == 'Final':
+            return 'матч завершен'
+        return status['detailedState']
+
+    @staticmethod
+    def _get_main_data(match_data, stat_dict):
+        live_data = match_data.get('liveData')
+        boxscore = live_data['boxscore']
+
+        home_stats = boxscore['teams']['home']['teamStats']['teamSkaterStats']
+        guest_stats = boxscore['teams']['away']['teamStats']['teamSkaterStats']
+
+        home_protocol = {'team': boxscore['teams']['home']['team']['name']}
+        guest_protocol = {'team': boxscore['teams']['away']['team']['name']}
+
+        for model_key, api_key in stat_dict.items():
+            home_protocol[model_key] = home_stats[api_key]
+            guest_protocol[model_key] = guest_stats[api_key]
+
+        return {
+            'row_home': home_protocol,
+            'row_guest': guest_protocol
+        }
+
+    @staticmethod
+    def _get_events_data(match_data, home_team, guest_team):
+        plays_data = match_data.get('liveData').get('plays').get('allPlays')
+
+        events = {
+            home_team: {},
+            guest_team: {}
+        }
+
+        for event in plays_data:
+            event_name = event['result']['event']
+            team = event.get('team')
+            if not team:
+                continue
+            event_team = team.get('name')
+
+            if event_name in events[event_team]:
+                events[event_team][event_name] += 1
+            else:
+                events[event_team][event_name] = 1
+
+        output = []
+        for team in (home_team, guest_team):
+            output.append({
+                'faceoff': events[team].get('Faceoff'),
+                'sh': sum((
+                    events[team].get('Shot') or 0,
+                    events[team].get('Goal') or 0,
+                    events[team].get('Blocked Shot') or 0,
+                    events[team].get('Missed Shot') or 0
+                )),
+            })
+        return {
+            'row_home': output[0],
+            'row_guest': output[1],
+        }
+
+    @staticmethod
+    def _get_score_by_period(match_data):
+        live_data = match_data.get('liveData')
+        linescore = live_data['linescore']
+
+        row_home = {}
+        row_guest = {}
+        for period in linescore['periods']:
+            num = period["num"]
+            if num == 4:
+                num = 'ot'
+            row_home[f'g_{num}'] = period['home']['goals']
+            row_guest[f'g_{num}'] = period['away']['goals']
+
+        # +1 гол выигравшему по буллитам
+        home_b = linescore.get('shootoutInfo').get('home').get('scores')
+        guest_b = linescore.get('shootoutInfo').get('away').get('scores')
+        row_home['g_b'] = 0
+        row_guest['g_b'] = 0
+        if home_b > guest_b:
+            row_home['g_b'] += 1
+        if home_b < guest_b:
+            row_guest['g_b'] += 1
+
+        return {
+            'row_home': row_home,
+            'row_guest': row_guest
+        }
