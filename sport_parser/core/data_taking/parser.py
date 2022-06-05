b2211d0fc7528cebd0523b3a992b3673fdc3d9c8
Dmitrij Svetov <bandodok@gmail.com>
2022-06-05 15:23:50 +0300

    created dataclasses for parsing methods

--- sport_parser/core/data_taking/parser.py
+++ sport_parser/core/data_taking/parser.py
@@ -1,11 +1,73 @@
 import json
 from abc import abstractmethod
 import os
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
+from typing import NamedTuple
+
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
 from sport_parser.core.data_analysis.formatter import Formatter
+
+
+class MatchStatus(Enum):
+    SCHEDULED = 'scheduled'
+    FINISHED = 'finished'
+    POSTPONED = 'postponed'
+    LIVE = 'live'
+    GAME_OVER = 'game over'
+
+
+@dataclass
+class TeamData:
+    name: str
+    season_num: int
+    img: str
+    city: str
+    arena: str
+    division: str
+    conference:  str
+
+
+@dataclass
+class MatchData:
+    id: int
+    season_num: int
+    date: datetime
+    status: MatchStatus
+    home_team_name: str
+    guest_team_name: str
+    penalties: bool = False
+    overtime: bool = False
+    arena: str = ''
+    city: str = ''
+    viewers: int = 0
+
+
+@dataclass
+class ProtocolRequiredStats:
+    g: int
+    g_1: int
+    g_2: int
+    g_3: int
+    g_ot: int
+    g_b: int
+
+
+@dataclass
+class ProtocolData:
+    team_name: str
+    required_stats: ProtocolRequiredStats
+    additional_stats: dict
+
+
+class MatchProtocolsData(NamedTuple):
+    match_id: int
+    home_protocol: ProtocolData
+    guest_protocol: ProtocolData
 
 
 class Parser:
