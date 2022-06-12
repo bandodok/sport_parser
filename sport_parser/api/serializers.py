44daf5ab6c31dbcee19e407cc6ee3a2b046e94a9
Dmitrij Svetov <bandodok@gmail.com>
2022-06-12 15:38:23 +0300

    updated serializers for new db structure

--- sport_parser/api/serializers.py
+++ sport_parser/api/serializers.py
@@ -2,19 +2,11 @@ from abc import ABC
 
 from rest_framework import serializers
 
-
-def get_sorted_teams_by_m2m_table(match):
-    meta = match._meta
-    app_label = meta.app_label
-    match_model = meta.model_name
-    team_model = meta.many_to_many[0].column
-    m2m_table = f'{app_label}_{match_model}_{team_model}'
-    team1, team2 = match.teams.all().extra(select={'add_id': f'{m2m_table}.id'}).order_by('add_id')
-    return team1, team2
+from sport_parser.core.models import MatchModel
 
 
 class CalendarSerializer(serializers.BaseSerializer, ABC):
-    def to_representation(self, match):
+    def to_representation(self, match: MatchModel):
         if match.status == 'finished':
             protocol1, protocol2 = match.protocols.all().order_by('id')
             return {
@@ -46,9 +38,10 @@ class CalendarSerializer(serializers.BaseSerializer, ABC):
                 'team2_image': protocol2.team.img,
                 'team2_id': protocol2.team.id,
             }
-        else:
-            team1, team2 = get_sorted_teams_by_m2m_table(match)
-            output = {
+        elif match.status in ('scheduled', 'postponed'):
+            team1 = match.home_team
+            team2 = match.guest_team
+            return {
                 'date': match.date,
                 'id': match.id,
                 'status': match.status,
@@ -58,18 +51,37 @@ class CalendarSerializer(serializers.BaseSerializer, ABC):
                 'team2_name': team2.name,
                 'team2_image': team2.img,
                 'team2_id': team2.id,
-                'match_data': match.live_data
             }
-            return output
+        else:
+            team1 = match.home_team
+            team2 = match.guest_team
+            return {
+                'date': match.date,
+                'id': match.id,
+                'status': match.status,
+                'live_status': match.live.status,
+                'team1_name': team1.name,
+                'team1_image': team1.img,
+                'team1_id': team1.id,
+                'team1_score': {
+                    'match': match.live.team1_score
+                },
+                'team2_name': team2.name,
+                'team2_image': team2.img,
+                'team2_id': team2.id,
+                'team2_score': {
+                    'match': match.live.team2_score
+                },
+            }
 
 
 class LiveMatchSerializer(serializers.BaseSerializer, ABC):
     def to_representation(self, match):
-        live_data = match.live_data
+        live_data = match.live
         return {
             'id': match.id,
-            'status': live_data.get('match_status', ''),
-            'team_1_score': live_data.get('team_1_score', ''),
-            'team_2_score': live_data.get('team_2_score', ''),
-            'data': live_data.get('data', '')
+            'status': live_data.status,
+            'team_1_score': live_data.team1_score,
+            'team_2_score': live_data.team2_score,
+            'data': live_data.bar_data
         }
